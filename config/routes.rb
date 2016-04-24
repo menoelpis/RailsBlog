79eff7c188b3dcdb10996c52078667ba47c4efc0
Daniel Park <menoelpis@gmail.com>
2016-04-25 07:50:00 +0900

    Added Specification and Moderator Controller with View

--- config/routes.rb
+++ config/routes.rb
@@ -48,9 +48,12 @@ Rails.application.routes.draw do
   #   resources :photos, concerns: :toggleable
 
   # Example resource route within a namespace:
-  #   namespace :admin do
+  # namespace :admin do
   #     # Directs /admin/products/* to Admin::ProductsController
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  namespace :admin do
+    resources :moderators, only: [:index]
+  end
 end
