63548c99c9425cc9c9596c180dadd3a35dc97759
Daniel Park <menoelpis@gmail.com>
2016-04-25 22:17:03 +0900

    Modified routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,8 @@
 Rails.application.routes.draw do
-  namespace :admin do
-  get 'sessions/new'
-  end
 
   namespace :admin do
-  get 'sessions/create'
-  end
-
-  namespace :admin do
-  get 'sessions/destroy'
-  end
-
-  namespace :admin do
+    resources :sessions, only: [:new, :create, :destroy]
     resources :moderators, only: [:index, :edit, :update]
   end
+  
 end
